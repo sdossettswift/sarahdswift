d32027f805f17db61c907b8055408ff1d3317195
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-10-05 20:21:15 -0500

    contact,school,skills

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,4 +1,19 @@
 class PortfolioController < ApplicationController
   def welcome
   end
+  
+  def skills
+  end
+
+  def schooling
+  end
+
+  def portfolio
+  end
+
+  def contact
+  end
+
+  def resume
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'resume/resume'
+
+  get 'skills' => 'portfolio#skills', as: :skills
+  get 'schooling' => 'portfolio#schooling', as: :schooling
+  get 'portfolio' =>'portfolio#portfolio', as: :portfolio
+  get 'contact' => 'portfolio#contact', as: :contact
+  get 'resume'=> 'portfolio#resume', as: :resume
+  get 'welcome'=> 'portfolio#welcome', as: :welcome
 
   root 'portfolio#welcome'
 end
