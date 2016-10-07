095be2c83a135327e0628dd34a25437b330a90a8
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-10-06 21:03:59 -0500

    remove bundle version in gemfile.lock

--- test/controllers/resume_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe ResumeController do
-  it "should get resume" do
-    get resume_resume_url
-    value(response).must_be :success?
-  end
-
-end
--- test/models/resume_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe Resume do
-  let(:resume) { Resume.new }
-
-  it "must be valid" do
-    value(resume).must_be :valid?
-  end
-end
